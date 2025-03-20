a66960054f8e816f684f2b5a47092d6d5299c158
Nikita-stud <nikita151998de@gmail.com>
2025-03-20 20:17:26 +0100

    ESLint installed and changed to globals

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: { ...globals.browser, module: true } },
+  },
+];
